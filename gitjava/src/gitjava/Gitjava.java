6fb08e5c394e547ec88e23fa9ce51942b26790eb
manana <alicialaborda13@gmail.com>
2022-06-14 10:03:05 +0200

    Cambiamos el dia

--- gitjava/src/gitjava/Gitjava.java
+++ gitjava/src/gitjava/Gitjava.java
@@ -16,6 +16,7 @@ public class Gitjava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hoy es Martes");
     }
     
 }
